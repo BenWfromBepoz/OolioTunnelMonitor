294f70d272bc48b5cfc70faa398b8ea1c09ef9f2
BenWfromBepoz <ben.w@bepoz.com.au>
2026-03-24 06:58:33 +1100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,21 +9,20 @@ namespace CloudflaredMonitor
         [STAThread]
         static void Main()
         {
-            // Single-instance guard: if already running, bring that window to front and exit
-            using var mutex = new Mutex(true, "CloudflaredMonitor_SingleInstance", out bool createdNew);
+            bool createdNew;
+            using var mutex = new Mutex(true, "TunnelMonitor_SingleInstance", out createdNew);
             if (!createdNew)
             {
-                // Signal the existing instance to show itself via a named event
-                using var evt = new EventWaitHandle(false, EventResetMode.AutoReset, "CloudflaredMonitor_ShowWindow");
-                evt.Set();
+                try
+                {
+                    using var evt = EventWaitHandle.OpenExisting("TunnelMonitor_ShowWindow");
+                    evt.Set();
+                }
+                catch { }
                 return;
             }
-
             ApplicationConfiguration.Initialize();
-
-            // Start minimised to system tray - don't show the main window on launch
-            var ctx = new TrayAppContext(startMinimised: true);
-            Application.Run(ctx);
+            Application.Run(new TrayAppContext(startMinimised: true));
         }
     }
 }
