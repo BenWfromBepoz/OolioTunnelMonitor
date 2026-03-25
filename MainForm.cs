def63fe59a6f0b2c44c5ee69677da9cdc3bf5b28
BenWfromBepoz <ben.w@bepoz.com.au>
2026-03-25 23:12:46 +1100

    Update MainForm.cs

--- MainForm.cs
+++ MainForm.cs
@@ -57,7 +57,7 @@ namespace CloudflaredMonitor
         
             int imageOffsetY = -20;
             int imageY = pad + imageOffsetY;
-            int spacing = -10; // negative = overlap, positive = gap
+            int spacing = -47; // negative = overlap, positive = gap
             int textY = imageY + spacing;
         
             g.DrawImage(_logo, new Rectangle(x, imageY, w, h));
