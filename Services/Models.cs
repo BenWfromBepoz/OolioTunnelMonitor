451516fcfd11a3e4985fc15c73d89f8a18168c23
BenWfromBepoz <ben.w@bepoz.com.au>
2026-03-29 19:09:34 +1100

    Rename namespace from CloudflaredMonitor to OolioTunnelMonitor

--- Services/Models.cs
+++ Services/Models.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
-namespace CloudflaredMonitor.Services
+namespace OolioTunnelMonitor.Services
 {
     internal sealed class TunnelServiceStatus
     {
