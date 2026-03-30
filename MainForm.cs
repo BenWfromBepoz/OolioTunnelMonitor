315ecfa5294a1854778dd0a21e3ea5bc15a0f19f
BenWfromBepoz <ben.w@bepoz.com.au>
2026-03-31 05:56:31 +1100

    Update MainForm.cs

--- MainForm.cs
+++ MainForm.cs
@@ -140,7 +140,24 @@ namespace OolioTunnelMonitor
             var rect = new Rectangle(0, py, pw, ph);
             var r = _pillColour.R; var gC = _pillColour.G; var b = _pillColour.B;
             Color baseCol = gC > r && gC > b ? Color.FromArgb(34, 197, 94) : r > gC && r > b ? Color.FromArgb(239, 68, 68) : Color.FromArgb(234, 179, 8);
-            using var grad = new LinearGradientBrush(new Point(0, py), new Point(pw, py + ph), ControlPaint.Light(baseCol, 0.3f), Color.FromArgb(14, 26, 119)); // ControlPaint.Dark(baseCol, 0.15f));
+            var blend = new ColorBlend // Build a smoother, offset gradient
+            {
+                Colors = new[]
+                {
+                    ControlPaint.Light(baseCol, 0.35f),  // brighter start
+                    ControlPaint.Light(baseCol, 0.25f),  // subtle shift
+                    ControlPaint.Light(baseCol, 0.15f),  // extended fade
+                    Color.FromArgb(14, 26, 119)          // your dark blue end
+                },
+                Positions = new[]
+                {
+                    0.0f,   // start
+                    0.5f,   // still mostly light
+                    0.66f,  // "centre" shifted right (~2/3)
+                    1.0f    // full dark
+                }
+            };
+            grad.InterpolationColors = blend;
             using var path = ShapeHelper.RoundedPath(rect, PillRadius);
             g.FillPath(grad, path);
             var glossRect = new Rectangle(0, py, pw, ph / 2);
